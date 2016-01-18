e77781ffe32c51674ce1f094588a3bc33a8a9d30
Thao Vo <tvo5@mail.pima.edu>
2016-01-18 04:26:37 -0700

    Commit message

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,22 @@
 Rails.application.routes.draw do
+    
+  resources :answers
+    # relates to all of the routes that has to do with question resources
+  resources :questions
+    
+    root 'home#index'
+    
+    get '/about' => 'home#about'
+    
+ #   post '/questions' => "home#temp"
+#    post '/answers' => "home#temp"
+   
+    
+ #   get '/questions/:id' => 'home#question'
+    
+    
+    
+    
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
