44f19a62a8bffba17e5e95e76db88801be967724
Thao Vo <tvo5@mail.pima.edu>
2016-01-19 00:48:32 -0700

    Commit message

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,13 +1,17 @@
 class AnswersController < ApplicationController
     
     def create 
-        
         question = Question.find(params[:answer][:question_id])
-        question.answers.create(answer_params)
+        answer = question.answers.create(answer_params)
+        
+        MainMailer.notify_question_author(answer).deliver_later
+        
+        session[:current_user_email] = answer_params[:email]
         redirect_to question
     end
     
     private
+    
     def answer_params
         params.require(:answer).permit(:question_id, :email, :body)
     end
--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,13 +2,12 @@ class QuestionsController < ApplicationController
   
     def show
         @question = Question.find(params[:id])
-        @answers = @question.answers.order(created_at: :desc)
-        
+        @answers = @question.answers.order(created_at: :desc)     
     end
     
     def create
         Question.create(question_params)
-        
+        session[:current_user_email] = question_params[:email]
         redirect_to root_path
     end
     
